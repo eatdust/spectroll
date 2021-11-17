244930ea68a3ca452a35bc2ea916613b1b6c8657
chris <chris@ringeval.com>
2021-11-18 00:16:00 +0100

    Remove debugging print

--- testrgb.py
+++ testrgb.py
@@ -2,15 +2,13 @@ import numpy as np
 import spectroll as sp
 
 
-def sRGB_linear_to_nonlinear(RGB):
-    gRGB=RGB
-    print(range(len(RGB)))
-    for i in range(len(RGB)):
-        print(RGB[i])
-        if RGB[i] <= 0.0031308:
-            gRGB[i] = RGB[i]*12.92
+def sRGB_linear_to_nonlinear(lRGB):
+    gRGB=lRGB
+    for i in range(len(lRGB)):
+        if lRGB[i] <= 0.0031308:
+            gRGB[i] = lRGB[i]*12.92
         else:
-            gRGB[i]=1.055*RGB[i]**(1.0/2.4)-0.055
+            gRGB[i]=1.055*lRGB[i]**(1.0/2.4)-0.055
             
     return gRGB
     
