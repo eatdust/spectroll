53a5879330a9af961eaee8731c99c5d21903d979
chris <chris@ringeval.com>
2021-11-18 00:14:25 +0100

    Add gamma correction in test

--- testrgb.py
+++ testrgb.py
@@ -2,6 +2,19 @@ import numpy as np
 import spectroll as sp
 
 
+def sRGB_linear_to_nonlinear(RGB):
+    gRGB=RGB
+    print(range(len(RGB)))
+    for i in range(len(RGB)):
+        print(RGB[i])
+        if RGB[i] <= 0.0031308:
+            gRGB[i] = RGB[i]*12.92
+        else:
+            gRGB[i]=1.055*RGB[i]**(1.0/2.4)-0.055
+            
+    return gRGB
+    
+
 D65 = sp.spectrum(name='D65',filename='data/illuminant_D65.dat')
 
 wavelength,white = D65.get_spectrum()
@@ -50,4 +63,6 @@ RHODW.set_RGB2XYZ_transforms(sp.sRGBchromaD65,Xw,Yw,Zw)
 
 print()
 print("White under night vision is: ")
-print("RHODW: RGB= ",RHODW.get_RGB())
+lRGB = RHODW.get_RGB()
+print("RHODW: lRGB= ",lRGB)
+print("RHODW: gRGB= ",sRGB_linear_to_nonlinear(lRGB))
